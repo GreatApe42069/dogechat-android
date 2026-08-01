89440b57e66bd21e4df4cf2909cc1713035f7d77
GreatApe42069 <Michaelharmon444@gmail.com>
2026-08-01 17:35:53 -0400

    Reorder plugin repositories and update includes
    Signed-off-by: GreatApe42069 <Michaelharmon444@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,19 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        // Guardian Project raw GitHub Maven (hosts info.guardianproject:arti-mobile-ex)
-        maven { url = uri("https://raw.githubusercontent.com/guardianproject/gpmaven/master") }
     }
 }
 
 rootProject.name = "dogechat-android"
 include(":app")
+include(":wear")
+// Using published Arti AAR; local module not included
